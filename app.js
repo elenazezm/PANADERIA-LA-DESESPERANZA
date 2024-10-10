f092336c35ac06b745c3a97205c65500e8584cc3
almaevs <user.email>
2024-10-09 18:08:22 -0600

    crud

---
+++ app.js
@@ -0,0 +1,82 @@
+const express = require("express")
+const mysql= require("mysql2")
+var bodyParser=require('body-parser')
+var app=express()
+var con=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'root',
+    database:'5IV8'
+})
+con.connect();
+
+app.use(bodyParser.json())
+
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+app.use(express.static('public'))
+
+app.post('/agregarUsuario',(req,res)=>{
+        let nombre=req.body.nombre
+        let id=req.body.id
+
+        con.query('INSERT INTO usuario (id_usuario, nombre) VALUES (?, ?)', [id, nombre], (err, respuesta, fields) => {
+            if (err) {
+                console.log("Error al conectar", err);
+                return res.status(500).send("Error al conectar");
+            }
+           
+            return res.send(`<h1>Nombre:</h1> ${nombre}`);
+        });
+   
+})
+
+app.listen(3000,()=>{
+    console.log('Servidor escuchando en el puerto 3000')
+})
+
+//fun consultar
+
+
+app.get('/obtenerUsuario',(req,res)=>{
+    con.query('select * from usuario', (err,respuesta, fields)=>{
+        if(err)return console.log('ERROR: ', err);
+        var userHTML=``;
+        var i=0;
+
+        respuesta.forEach(user => {
+            i++;
+            userHTML+= `<tr><td>${i}</td><td>${user.nombre}</td></tr>`;
+
+
+        });
+
+        return res.send(`<table>
+                <tr>
+                    <th>id</th>
+                    <th>Nombre:</th>
+                <tr>
+                ${userHTML}
+                </table>`
+        );
+
+
+    });
+});
+
+app.post('/borrarUsuario', (req, res) => {
+    const id = req.body.id; // El ID del usuario a eliminar viene en el cuerpo de la solicitud
+    console.log("hola")
+    con.query('DELETE FROM usuario WHERE id_usuario = ?', [id], (err, resultado, fields) => {
+
+        if (err) {
+            console.error('Error al borrar el usuario:', err);
+            return res.status(500).send("Error al borrar el usuario");
+        }
+        if (resultado.affectedRows === 0) {
+            return res.status(404).send("Usuario no encontrado");
+        }
+        return res.send(`Usuario con ID ${id} borrado correctamente`);
+    });
+});
